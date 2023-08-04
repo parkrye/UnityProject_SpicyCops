68cacabb11afcde54c436885dc982a1f5e5bc5b5
전성원 <gateoper1105@gmail.com>
2023-08-04 21:24:37 +0900

    feat : Enemy, Map fix
    Enemy의 구성 추가 구현 및 Player와 상호작용, Map fix, Map 일부 상호작용 fix

--- Assets/Jeon/Scripts/Enemy/Enemy.cs
+++ Assets/Jeon/Scripts/Enemy/Enemy.cs
@@ -4,6 +4,7 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.UI;
+using static UnityEditorInternal.VersionControl.ListControl;
 
 namespace Jeon
 {
@@ -12,43 +13,36 @@ namespace Jeon
         private Rigidbody rb;
         private NavMeshAgent agent;
         private Vector3 followPos;
-        private Animator anim;
+        public Animator anim;
 
         Dictionary<string, int> playerState = new Dictionary<string, int>();
 
         [SerializeField] Transform players;
         [SerializeField] Transform catchZone;
-        [SerializeField] float moveSpeed;
-        [SerializeField] float maxSpeed;
         [SerializeField] float targetFindTime;
 
         public enum EnemyState { Idle, Follow, Angry, Berserker, Catch}
 
         private EnemyState curState;
+        private EnemyState lastState;
 
-        private float curTime;            // 현재시간은 게임내에서 시간을 적용시킨다 약 : 처음 시간은 180초
+        [SerializeField] float curTime;            // 현재시간은 게임내에서 시간을 적용시킨다 약 : 처음 시간은 180초
+        [SerializeField] float curSpeed;
 
-        private void Awake()
+        public void Awake()
         {
             rb = GetComponent<Rigidbody>();
             agent = GetComponent<NavMeshAgent>();
             anim = GetComponent<Animator>();
             //catchZone = GameObject.Find("CatchZone").transform;
             curTime = 180f;      // time = InGameManager에서 받아오기
-            curState = EnemyState.Idle;
+            curSpeed = agent.speed;
         }
-
-        private void FixedUpdate()
+        private void Start()
         {
             players = GameObject.FindGameObjectWithTag("Player").transform;
-            agent.destination = players.transform.position;
 
-            if (curTime == 175f)
-            {
-                agent.destination = players.transform.position;
-
-            }
-            
+            StartCoroutine(FinedPlayer());
         }
 
         private void Update()
@@ -83,24 +77,23 @@ namespace Jeon
         }
         private void DoIdle()
         {
+            
             // 게임을 시작할 때 잠깐 몇초동안 가만히 있는다
-            if (curTime == 175f)
+            if (curTime <= 175f)
             {
-                // 애니메이션 실행해주기
-                anim.SetBool("WalkTime", true);
                 curState = EnemyState.Follow;
                 
             }
+            StopCoroutine(FinedPlayer());
         }
         private void DoFollow()
         {
+            anim.SetBool("WalkTime", true);
+            agent.speed = 3f;
+
             // Player중에 어그로수치가 가장 높은 player를 쫒는다
-            if (curTime == 120f)
+            if (curTime <= 120f)
             {
-                // 애니메이션 실행해주기
-                anim.SetBool("WalkTime", false);
-                anim.SetBool("RunningTime", true);
-                agent.speed = 6f;
                 curState = EnemyState.Angry;
 
             }
@@ -109,35 +102,57 @@ namespace Jeon
         private void DoAngry()
         {
             // 이동속도를 증가시키고 어그로수치가 가장 높은 player를 쫒는다
-            // IEnumerator angry;
-            if (curTime == 60f)
+            agent.speed = 3.5f;
+            anim.SetBool("WalkTime", false);
+            anim.SetBool("RunningTime", true);
+
+            if (curTime <= 60f)
             {
-                // 애니메이션 실행해주기
-                agent.speed = 8f;
                 curState = EnemyState.Berserker;
             }
         }
 
         private void DoBerserker()
         {
-            // 이동속도를 더욱 더 증가시키고 어그로수치가 가장 높은 player를 쫒는다
-
-            // 애니메이션 실행해주기
-            if (curTime == 15f)
+            agent.speed = 4f;
+            if (curTime <= 15f)
             {
-                
-                agent.speed = 12f;
+                agent.speed = 6f;
             }
-            if (curTime == 0)
+            else if (curTime <= 0)
             {
                 agent.speed = 0f;
+                StopAllCoroutines();
             }
         }
 
         private void DoCatch()
         {
-            // 사정거리를 만들어서 안에 들어오면 anim.SetBool("Area", true);로 애니메이션 실행과 플레이어 사망
+            curSpeed = agent.speed;
+            lastState = curState;
 
+            agent.speed = 0.1f;
+        }
+        
+        private void Remove()
+        {
+            curState = EnemyState.Idle;
+            agent.speed = curSpeed;
+            curState = lastState;
+        }
+
+        IEnumerator FinedPlayer()
+        {
+            yield return new WaitForSeconds(5f);
+            
+            curState = EnemyState.Idle;
+            curTime = 175f;
+            yield return null;
+            while (true)
+            {
+                agent.destination = players.transform.position;
+                yield return new WaitForSeconds(0.5f);
+            }
         }
     }
 
