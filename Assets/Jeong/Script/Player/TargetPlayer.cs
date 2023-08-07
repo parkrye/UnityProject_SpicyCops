3a4e58eec50abcaa0fac2432fca9721d2e3cf7e0
Jeong Seong Jin <wjdtjdwls95@naver.com>
2023-08-07 12:03:02 +0900

    fixed. Fixing Player Pull
    플레이어 당기기 수정중

--- Assets/Jeong/Script/Player/TargetPlayer.cs
+++ Assets/Jeong/Script/Player/TargetPlayer.cs
@@ -4,6 +4,28 @@ using UnityEngine;
 
 public class TargetPlayer : MonoBehaviour
 {
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerPuller puller = other.GetComponent<PlayerPuller>();
+            if (puller != null)
+            {
+                puller.SetPullTarget(gameObject); // PlayerPuller의 새 메서드 호출하여 PullTarget 설정
+            }
+        }
+    }
 
- 
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerPuller puller = other.GetComponent<PlayerPuller>();
+            if (puller != null)
+            {
+                puller.ClearPullTarget(); // PlayerPuller의 새 메서드 호출하여 PullTarget 해제
+            }
+        }
+    }
+
 }
