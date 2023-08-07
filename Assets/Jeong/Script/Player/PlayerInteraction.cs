e0f74aca739e8bd3ff15dbf3038dfc2333fedbc6
wjdtjdwls95 <wjdtjdwls95@naver.com>
2023-08-07 19:36:42 +0900

    feat. Create Playher acquire and use item
    플레이어 아이템획득 및 사용 기능 구현

--- Assets/Jeong/Script/Player/PlayerInteraction.cs
+++ Assets/Jeong/Script/Player/PlayerInteraction.cs
@@ -11,25 +11,37 @@ public class PlayerInteraction : MonoBehaviour
     [SerializeField] Transform point;
     [SerializeField] float range;
 
+    private bool canInteract = false;
+
     private Animator anim;
+
 
     private void Awake()
     {
         anim = GetComponent<Animator>();
     }
 
-    public void Interact()
+
+    // 상호작용 오브젝트 탐색
+    public void FindInteract()
     {
         
-        Collider[] colliders = Physics.OverlapSphere(point.position, range);
-        foreach (Collider collider in colliders)
+    }
+
+    // 상호작용
+    public void Interact()
+    {
+        if (canInteract)
         {
-            IInteractable interactable = collider.GetComponent<IInteractable>();
-            interactable?.Interact();
-            anim.SetTrigger("IsPicked");
+            Collider[] colliders = Physics.OverlapSphere(point.position, range);
+            foreach (Collider collider in colliders)
+            {
+                IInteractable interactable = collider.GetComponent<IInteractable>();
+                interactable?.Interact();
+                anim.SetTrigger("IsPicked");
+            }
+            Debug.Log("Player Interact");
         }
-        Debug.Log("Player Interact");
-        
     }
 
     private void OnInteract(InputValue value)
@@ -42,7 +54,7 @@ public class PlayerInteraction : MonoBehaviour
         if (!debug)
             return;
 
-        Gizmos.color = Color.black;
+        Gizmos.color = Color.magenta;
         Gizmos.DrawWireSphere(point.position, range);
     }
 
