cb41644046784adcc33df512b03053bce9a34130
전성원 <gateoper1105@gmail.com>
2023-08-08 20:01:47 +0900

    feat : PlayerDied Motion, Enemy
    Enemy Photon 연동 및 수정, Player죽는모션 추가 후 Enemy와 합

---
+++ Assets/Jeon/Scripts/Player/PlayerDied.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDied : MonoBehaviour
+{
+    Animator animator;
+    [SerializeField] InGameManager gameManager;
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void DoDeath()
+    {
+        animator.SetBool("IsDead", true);
+        // 경륜이한테 InGameManager받아오면 PlayerAliveDictionary false로
+    }
+}
