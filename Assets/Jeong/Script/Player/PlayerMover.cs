a69041a6623331d7bff16c77efabdb9230033db7
wjdtjdwls95 <wjdtjdwls95@naver.com>
2023-08-08 19:51:22 +0900

    feat. Create Player Push and Pull function
    Player Push 및 Pull 기능 구현

--- Assets/Jeong/Script/Player/PlayerMover.cs
+++ Assets/Jeong/Script/Player/PlayerMover.cs
@@ -1,83 +1,100 @@
 using Photon.Pun;
-using Photon.Pun.Demo.Procedural;
-using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.UIElements;
-using static UnityEditor.ShaderGraph.Internal.KeywordDependentCollection;
 
 public class PlayerMover : MonoBehaviourPun
 {
-    [SerializeField] public float moveSpeed; // 이동속도
-    [SerializeField] private float maxSpeed; // 최대 이동속도
 
+    [SerializeField] public float curSpeed = 0f;
+    [SerializeField] public float moveSpeed; // 이동속도
     [SerializeField] public float rotateSpeed; // 회전속도
 
-    private float curSpeed = 0f;
-
     private Animator anim;
+    private CharacterController controller;
     private Rigidbody rigid;
-    private PlayerInput playerInput;
 
-    private Vector2 inputDir;
+    [SerializeField] private Transform lookRotation;
+
+    private Vector3 inputDir;
     private float ySpeed;
 
     private bool isPulling = false; // PlayerPuller 클래스로부터 전달받은 값
 
     private void Start()
     {
-        rigid = GetComponent<Rigidbody>();
+        
         anim = GetComponent<Animator>();
-        playerInput = GetComponent<PlayerInput>();
+        controller = GetComponent<CharacterController>();
+        rigid = GetComponent<Rigidbody>();
 
-        if (!photonView.IsMine)
-            Destroy(playerInput);
+        lookRotation.position = new Vector3(0, 0, 0.2f);
     }
-
+    private void Update()
+    {
+        
+    }
     private void FixedUpdate()
     {
+        
         Move();
+        //lookRotation.localPosition = (new Vector3(inputDir.x + transform.position.x, 0, inputDir.z + transform.position.z) + transform.forward * 0.2f);
+
     }
 
     private void Move()
     {
-        Vector3 moveDir = new Vector3(inputDir.x, 0, inputDir.y);
-
+        if ( inputDir.magnitude == 0)
+        {
+            curSpeed = Mathf.Lerp(curSpeed, 0, 0.9f);
+            anim.SetFloat("IsMoved", curSpeed);
+            return;
+        }
+        
+        Vector3 fowarVec = new Vector3(Vector3.forward.x, 0, Vector3.forward.z);
+        Vector3 rightVec = new Vector3(Vector3.right.x, 0, Vector3.right.z);
         ySpeed += Physics.gravity.y * Time.deltaTime;
-        rigid.velocity = moveDir * moveSpeed * Time.deltaTime + Vector3.up * rigid.velocity.y;
+        controller.Move(Vector3.up * ySpeed * Time.deltaTime);
 
-        curSpeed = rigid.velocity.magnitude;
+        if (inputDir != null)
+        {
+            curSpeed = Mathf.Lerp(curSpeed, moveSpeed, 0.1f);
+        }
 
         // 회전
-        if (moveDir != Vector3.zero)
-        {
-            // 이동 방향으로 회전 (잡기상태가 아닐떄)
-            if(!isPulling)
-            transform.forward = Vector3.Lerp(transform.forward, moveDir, rotateSpeed * Time.deltaTime);
-        }
 
-        // 최대속력
-        if (rigid.velocity.sqrMagnitude > maxSpeed)
-        {
-            rigid.velocity = rigid.velocity.normalized * maxSpeed;
-        }
+         transform.LookAt(transform.position + Vector3.forward * inputDir.z + Vector3.right * inputDir.x);
+
+        //if (fowarVec != Vector3.zero && rightVec != Vector3.zero)
+        //{
+        //    // 이동 방향으로 회전 (잡기상태가 아닐떄)
+        //    if (!isPulling)
+        //    {/*
+        //        Quaternion lookRotation = Quaternion.Euler(rightVec.x, 0, fowarVec.z);
+        //        lookRotation.eulerAngles = new Vector3();
+        //        transform.rotation = lookRotation;*/
+
+        //        transform.LookAt(lookRotation);
+        //    }
+        //}
+
+        controller.Move(fowarVec * inputDir.z * moveSpeed * Time.deltaTime);
+        controller.Move(rightVec * inputDir.x * moveSpeed * Time.deltaTime);
 
         anim.SetFloat("IsMoved", curSpeed);
+
+        
     }
 
     private void OnMove(InputValue value)
     {
-        inputDir = value.Get<Vector2>();
+        inputDir.x = value.Get<Vector2>().x;
+        inputDir.z = value.Get<Vector2>().y;
+
+        
     }
 
     public void SetMoveSpeed(float speed)
     {
         moveSpeed = speed;
     }
-
-  
-
 }
