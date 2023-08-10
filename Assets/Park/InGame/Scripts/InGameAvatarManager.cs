0ddc216708568a939b42e653795753603eacb8eb
박경륜 <parkrye@gmail.com>
2023-08-10 15:59:24 +0900

    feat: 캐릭터 아바타 변경 기초

--- Assets/Park/InGame/Scripts/InGameAvatarManager.cs
+++ Assets/Park/InGame/Scripts/InGameAvatarManager.cs
@@ -4,18 +4,15 @@ using UnityEngine;
 public class InGameAvatarManager : CharacterSkinManager
 {
     [SerializeField] List<GameObject> avatars;
+    [SerializeField] SkinnedMeshRenderer skinnedMeshRenderer;
     [SerializeField] int avatarNum;
     
     public void Initialize(int _avatar, int _color)
     {
+        avatars = new List<GameObject>();
         avatarNum = _avatar;
 
-        for (int i = 0; i < GameData.AVATAR.Count; i++)
-        {
-            avatars.Add(Resources.Load<GameObject>($"Avatar/{GameData.AVATAR[i]}"));
-        }
-
-        for (int i = 0; i < avatars.Count; i++)
+        for(int i = 0; i < avatars.Count; i++)
         {
             avatars[i].SetActive(i == avatarNum);
         }
