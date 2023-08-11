6ceebc3f5fa82eb469d94e86a68da1caf27b823b
전성원 <gateoper1105@gmail.com>
2023-08-11 20:38:30 +0900

    fix : Enemy Animation Bug Fix

--- Assets/Jeon/Scripts/Enemy/HitBox.cs
+++ Assets/Jeon/Scripts/Enemy/HitBox.cs
@@ -8,12 +8,10 @@ public class HitBox : MonoBehaviourPun
 {
     [SerializeField] private PoliceEnemy enemy;
     [SerializeField] InGameManager inGameManager;
-    [SerializeField] List<int> playerViewIdList;
 
     private void Awake()
     {
         enemy = GetComponentInParent<PoliceEnemy>();
-        playerViewIdList = new();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -21,50 +19,16 @@ public class HitBox : MonoBehaviourPun
         Debug.Log(other.gameObject.name + other.gameObject.layer);
         if (other.gameObject.layer == 3)
         {
-            photonView.RPC("RequestAddPlayer", RpcTarget.MasterClient, other.GetComponent<PhotonView>().ViewID);
+            enemy.AddPlayer(other.GetComponent<PhotonView>().ViewID);
         }
     }
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject.layer == 3)
         {
-            photonView.RPC("RequestExitPlayer", RpcTarget.MasterClient, other.GetComponent<PhotonView>().ViewID);
+            enemy.RemovePlayer(other.GetComponent<PhotonView>().ViewID);
         }
 
     }
-    [PunRPC]
-    private void RequestAddPlayer(int playerId)
-    {
-        playerViewIdList.Add(playerId);
-        Debug.Log($"{playerId}");
-        photonView.RPC("RequestHoldPlayer", RpcTarget.MasterClient, playerId);
-    }
-
-    [PunRPC]
-    private void RequestHoldPlayer(int playerId)
-    {
-        foreach (int view in playerViewIdList)
-        {
-            if (view == playerId)
-            {
-                Debug.Log($"{playerId}");
-                enemy.Anim.SetBool("InArea", true);
-                // Debug.Log(enemy.Anim.GetBool("InArea"));
-                enemy.playerTransform[view].GetComponent<PlayerDied>().DoDeath();
-                inGameManager.PlayerDead(view);
-                enemy.playerTransform[view].GetComponent<PlayerInput>().enabled = false;
-                StartCoroutine(EnemyAttackStop());
-            }
-        }
-    }
-    [PunRPC]
-    private void RequestExitPlayer(int playerId)
-    {
-        playerViewIdList.Remove(playerId);
-    }
-    IEnumerator EnemyAttackStop()
-    {
-        yield return new WaitForSeconds(1f);
-        enemy.Anim.SetBool("InArea", false);
-    }
+    
 }
